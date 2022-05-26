c0e45d3091cd9119e275c81f737928fa833c04d6
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2022-05-26 17:16:41 -0300

    iniciando com NodeJs

---
+++ Node/moduloA.js
@@ -0,0 +1,6 @@
+// o ola(atributo) está sendo criado dinamicamente e recebendo algo
+this.ola = 'Fala Pessoal'
+// estou criando um atributo dentro do objeto exports que está recebendo o valor 'Bem vindo...'
+exports.bemVindo = 'Bem vindo ao Node.JS'
+
+module.exports.ateLogo = 'Até o próximo exemplo'
---
+++ Node/moduloB.js
@@ -0,0 +1,9 @@
+// Chamei o exports por meio do module e atribuí um objeto a ele
+module.exports = {
+  // padrão CHAVE=>VALOR
+  bomDia: 'Bom dia',
+  // tbm posso atribuir funções
+  boaNoite() {
+    return 'Boa noite'
+  }
+}
