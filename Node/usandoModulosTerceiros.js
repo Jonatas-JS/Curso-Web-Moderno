f7e2ff6ef70a07bd66e197b5773c6f70226a6a77
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2022-05-27 15:46:14 -0300

    usando módulos de terceiros

---
+++ Node/usandoModulosTerceiros.js
@@ -0,0 +1,5 @@
+// criado constante _ para receber as funcionalidades da requisição do lodash
+const _ = require('lodash')
+//setIterval => (valor, intervaloTempo)
+//(_.random(de, até)) => acessando uma função da biblioteca lodash que gera números aleatórios.
+setInterval(() => console.log(_.random(1, 100)), 2000)
